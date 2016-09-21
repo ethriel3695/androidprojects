4ac7539e40cc895cc9456570670a568c72e663ca
Reuben Ellis <ethriel3695@gmail.com>
2016-09-21 07:43:59 -0600

    Phoenix Application

---
+++ PhoenixApplication/app/src/main/java/com/elllistech/phoenixapplication/UserInfoDisplayActivity.java
@@ -0,0 +1,13 @@
+package com.elllistech.phoenixapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInfoDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info_display);
+    }
+}
