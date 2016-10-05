4f218a0ec9337912c1d571da12ebdb14701b4f3a
Reuben Ellis <ethriel3695@gmail.com>
2016-10-04 23:03:41 -0600

    Added Spinner Drop downs with populated records Added TODO's to create a fragment for scores or a message indicating the Teams did not play each other

--- NFLScorerApplication/app/src/main/java/com/elllistech/nflscorerapplication/NFLScoringActivity.java
+++ NFLScorerApplication/app/src/main/java/com/elllistech/nflscorerapplication/NFLScoringActivity.java
@@ -1,7 +1,14 @@
 package com.elllistech.nflscorerapplication;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import static android.R.attr.id;
 
 public class NFLScoringActivity extends AppCompatActivity {
 
@@ -9,5 +16,33 @@ public class NFLScoringActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nflscoring);
+
+        ActionBar
+                titleBar = getSupportActionBar();
+
+        titleBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+
+        Spinner
+                eastTeamsSpinner = (Spinner)findViewById(R.id.spinnerEastTeams),
+                westTeamsSpinner = (Spinner)findViewById(R.id.spinnerWestTeams);
+
+        ArrayAdapter<CharSequence>
+                westTeamsAdapter = ArrayAdapter.createFromResource(this, R.array.westTeams,
+                    R.layout.support_simple_spinner_dropdown_item),
+                eastTeamsAdapter = ArrayAdapter.createFromResource(this, R.array.eastTeams,
+                    R.layout.support_simple_spinner_dropdown_item);
+        westTeamsSpinner.setAdapter(westTeamsAdapter);
+        eastTeamsSpinner.setAdapter(eastTeamsAdapter);
+
+        //TODO: Create a fragment which populates the Score if the teams played
+        //TODO: Create a fragment which populates the text "DID NOT PLAY" if the teams did not play
+//        String[]
+//            eastTeams = {"New England Patriots", "Green Bay Packers",
+//                "Chicago Bears", "Minnesota Vikings"},
+//            westTeams = {"Denver Broncos", "Seattle Seahawks",
+//                    "Los Angeles Rams", "Arizona Cardinals"};
+
+
+
     }
 }
